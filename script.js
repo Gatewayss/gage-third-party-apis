218dd399f079e512454f49cf475c1d7080ec25a2
Gage Norton <nortongage0@gmail.com>
2023-01-12 22:26:13 -0600

    Add current time and day feature to header

--- script.js
+++ script.js
@@ -18,5 +18,7 @@ window.onload = $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
+   let currentDayAndTime = dayjs()
+   $('#currentDay').text(currentDayAndTime)
 });
