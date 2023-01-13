390a83c6a02d1f93fa3a9540899b2be348cae94e
Gage Norton <nortongage0@gmail.com>
2023-01-12 23:54:10 -0600

    Create local storge key pair for id buttons

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 $(document).ready(function () {
   $(function app() {
-    // TODO: Add a listener for click events on the save button. This code should
+
+    // adds event listener to all save buttons 
+    $('.saveBtn').on('click', (event) => {
+    // grabs the button clicked 
+      let curButton = $(event.currentTarget);
+    //grabs the id of the button clicked 
+      let curButtonId = $(curButton).attr('id')
+      localStorage.setItem("button Id", curButtonId)
+    })
+    // This code should
     // use the id in the containing time-block as a key to save the user input in
     //});
     // local storage. HINT: What does `this` reference in the click listener
