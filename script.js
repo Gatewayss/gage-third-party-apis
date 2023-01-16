649bcf1a88a35029bf3ce3000eacdab0e379905b
Gage Norton <nortongage0@gmail.com>
2023-01-15 21:58:06 -0600

    Remove comments

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
   $(function app() {
+    // current time and day in the header 
+    const currentDayAndTime = dayjs()
+    $('#currentDay').text(currentDayAndTime)
 
     // adds event listener to all save buttons 
     $('.saveBtn').on('click', (event) => {
-    // grabs the button clicked 
+      // grabs the button clicked 
       const curButton = $(event.currentTarget);
       // grabs the id of the button clicked 
       const curButtonId = $(curButton).attr('id')
@@ -17,37 +20,27 @@ $(document).ready(function () {
       localStorage.setItem(curButtonId, valueOfTextarea)
     })
 
-      // saves the text content to the associated container  
-      const hour9Text = localStorage.getItem('hour-9')
-      $('#hour-9-text').text(hour9Text)
-      const hour10Text = localStorage.getItem('hour-10')
-      $('#hour-10-text').text(hour10Text)
-      const hour11Text = localStorage.getItem('hour-11')
-      $('#hour-11-text').text(hour11Text)
-      const hour12Text = localStorage.getItem('hour-12')
-      $('#hour-12-text').text(hour12Text)
-      const hour1Text = localStorage.getItem('hour-1')
-      $('#hour-1-text').text(hour1Text)
-      const hour2Text = localStorage.getItem('hour-2')
-      $('#hour-2-text').text(hour2Text)
-      const hour3Text = localStorage.getItem('hour-3')
-      $('#hour-3-text').text(hour3Text)
-      const hour4Text = localStorage.getItem('hour-4')
-      $('#hour-4-text').text(hour4Text)
-      const hour5Text = localStorage.getItem('hour-5')
-      $('#hour-5-text').text(hour5Text)
-     
-      
-    
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    
-    
-    const currentDayAndTime = dayjs()
-    $('#currentDay').text(currentDayAndTime)
+    // saves the text content to the associated container  
+    const hour9Text = localStorage.getItem('hour-9')
+    $('#hour-9-text').text(hour9Text)
+    const hour10Text = localStorage.getItem('hour-10')
+    $('#hour-10-text').text(hour10Text)
+    const hour11Text = localStorage.getItem('hour-11')
+    $('#hour-11-text').text(hour11Text)
+    const hour12Text = localStorage.getItem('hour-12')
+    $('#hour-12-text').text(hour12Text)
+    const hour1Text = localStorage.getItem('hour-1')
+    $('#hour-1-text').text(hour1Text)
+    const hour2Text = localStorage.getItem('hour-2')
+    $('#hour-2-text').text(hour2Text)
+    const hour3Text = localStorage.getItem('hour-3')
+    $('#hour-3-text').text(hour3Text)
+    const hour4Text = localStorage.getItem('hour-4')
+    $('#hour-4-text').text(hour4Text)
+    const hour5Text = localStorage.getItem('hour-5')
+    $('#hour-5-text').text(hour5Text)
+
+    let currentHour = new Date().getHours();
+    console.log(currentHour);
   });
 });
