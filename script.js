3e3b123d879e53e1d355eb733eb723c913010122
Gage Norton <nortongage0@gmail.com>
2023-01-15 20:10:49 -0600

    Add text to new from local to storage to new time blocks

--- script.js
+++ script.js
@@ -24,6 +24,19 @@ $(document).ready(function () {
       $('#hour-10-text').text(hour10Text)
       const hour11Text = localStorage.getItem('hour-11')
       $('#hour-11-text').text(hour11Text)
+      const hour12Text = localStorage.getItem('hour-12')
+      $('#hour-12-text').text(hour12Text)
+      const hour1Text = localStorage.getItem('hour-1')
+      $('#hour-1-text').text(hour1Text)
+      const hour2Text = localStorage.getItem('hour-2')
+      $('#hour-2-text').text(hour2Text)
+      const hour3Text = localStorage.getItem('hour-3')
+      $('#hour-3-text').text(hour3Text)
+      const hour4Text = localStorage.getItem('hour-4')
+      $('#hour-4-text').text(hour4Text)
+      const hour5Text = localStorage.getItem('hour-5')
+      $('#hour-5-text').text(hour5Text)
+     
       
     
     // TODO: Add code to apply the past, present, or future class to each time
