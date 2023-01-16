f19285da17053c8d2e50b67cf1c9c8223fafe397
Gage Norton <nortongage0@gmail.com>
2023-01-15 19:59:56 -0600

    Remove comments

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
   $(function app() {
 
@@ -27,31 +25,14 @@ $(document).ready(function () {
       const hour11Text = localStorage.getItem('hour-11')
       $('#hour-11-text').text(hour11Text)
       
-      
     
-   /*
-this part:
-2. loop through the array of time block input elements and inside the loop:
-    a. do localstorage.getItem('hour-9') (for example)
-    b. if that value exists, make the textcontent equal to it
-    c. do nothing if the value doesn't exist
-   */
-  
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
+    
     
     const currentDayAndTime = dayjs()
     $('#currentDay').text(currentDayAndTime)
